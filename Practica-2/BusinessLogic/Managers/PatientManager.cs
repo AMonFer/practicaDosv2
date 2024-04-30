7e81593195f637bc04f018c43343c594dd212ecd
adrian <ferrufinoadrian70@gmail.com>
2024-04-30 09:01:36 -0400

    P3-001 agregue la library class, Patiet, Patient Manager, create patient function and random bloodtype for the patient

---
+++ Practica-2/BusinessLogic/Managers/PatientManager.cs
@@ -0,0 +1,60 @@
+﻿using BusinessLogic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPB.BusinessLogic.Managers
+{
+    internal class PatientManager
+    {
+        public PatientManager() { }
+
+        public Patient CrearPatient(string name, string lastname, int CI) {
+            string sangre = randomBlood();
+
+            Patient patient = new Patient()
+            {
+                Name = name,
+                LastName = lastname,
+                CI = CI,
+                BloodType = sangre
+            };
+            return patient;
+        }
+        public string randomBlood() { 
+            Random rnd = new Random();
+            int random = rnd.Next(1, 81);
+            if (random < 10) {
+                return "A+";
+            }
+            if (random < 20) {
+                return "A-";
+            }
+            if (random < 30) {
+                return "B+";
+            }
+            if (random < 40) {
+                return "B-";
+            }
+            if (random < 50) {
+                return "AB+";
+            }
+            if (random < 60)
+            {
+                return "AB-";
+            }
+            if (random < 70)
+            {
+                return "O+";
+            }
+            if (random < 80)
+            {
+                return "O-";
+            }
+            return "O+";
+
+        }
+    }
+}
---
+++ Practica-2/BusinessLogic/Models/Patient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Models
+{
+    public class Patient
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int CI { get; set; }
+        public string BloodType { get; set; }
+        public Patient() { }
+        public Patient(string Name, string LastName, int CI, string BloodType) { 
+            this.Name = Name;
+            this.LastName = LastName;
+            this.CI = CI;
+            this.BloodType = BloodType;
+        }
+    }
+}
