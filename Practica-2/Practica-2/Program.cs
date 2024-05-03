65334c8cf7dd7f0510d22000e26f0cdf72db29c0
adrian <ferrufinoadrian70@gmail.com>
2024-05-03 10:53:05 -0400

    P3-001 implemented exceptions for the config files

--- Practica-2/Practica-2/Program.cs
+++ Practica-2/Practica-2/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.OpenApi.Models;
 using Serilog;
 using UPB.BusinessLogic.Managers;
+using UPB.BusinessLogic.Managers.Exceptions;
 using UPB.Practica_2.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,7 +14,11 @@ builder.Configuration
     .Build();
 
 
-
+string titulo = builder.Configuration.GetSection("ConnectionStrings").GetSection("titulo").Value;
+if (titulo == null) {
+    titulo = "WebAPI de Adrian";
+    throw new PracticeException("No se pudo obtener el titulo del archivo de configuracion y devolvio nulo");
+}
 
 // Add services to the container.
 
@@ -21,14 +26,22 @@ builder.Services.AddControllers();
 builder.Services.AddTransient<PatientManager>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo() {
+        Title = titulo
+    });
+});
 
 var app = builder.Build();
 
 //app.UseExceptionHandlerMiddleware();
 
 string direccionLog = builder.Configuration.GetSection("Logging").GetSection("FileLocation").Value;
-string titulo = builder.Configuration.GetSection("ConnectionStrings").GetSection("titulo").Value;
+if (direccionLog == null)
+{
+    direccionLog = "../logs";
+    throw new PracticeException("No se pudo obtener la direccion para guardar los logs y devolvio null");
+}
 
 app.UseSwaggerUI(c =>
 {
